5dfdf776e4a7d18f569431645ff4ab1c098abc17
ss49919201 <sakashin10291029@gmail.com>
2024-09-13 23:08:36 +0900

    chore: hotokes sql

---
+++ hotokes.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS hotokes (
+  id VARCHAR(50),
+  name VARCHAR(50),
+  created_at DATE
+);
+INSERT INTO hotokes (id, name, created_at) VALUES ('01GREFXCN9519NRVXWTPG0V0BF', '阿弥陀仏', '2022-08-20 05:39:52');
+INSERT INTO hotokes (id, name, created_at) VALUES ('01GREFXCNBYBGX2GC6ZGY9FMP4', '大日如来', '2022-12-15 21:56:13');
+INSERT INTO hotokes (id, name, created_at) VALUES ('01GREFXCNCWAJWRQWC2863MYW4', '釈迦牟尼仏', '2022-07-28 04:13:37');
