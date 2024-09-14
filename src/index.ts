2045510dae4ed58ea4e6244fa8eea6dd231dad85
ss49919201 <sakashin10291029@gmail.com>
2024-09-14 10:09:18 +0900

    feat: load many from kv

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import { Hono } from "hono";
 import { Env, newLoadManyHotokes } from "./infrastructure/d1/repository/hotoke";
 
-const app = new Hono<{ Bindings: Env }>();
+import {
+  Env as EnvWithKv,
+  newLoadManyHotokes as newLoadManyHotokesWithKv,
+} from "./infrastructure//kv/reposiotry/hotoke";
+
+const app = new Hono<{ Bindings: Env & EnvWithKv }>();
 
 app.get("/", async (c) => {
   const loadManyHotokes = newLoadManyHotokes(c.env);
+  const hotokes = await loadManyHotokes({});
+  return c.json(hotokes);
+});
+
+app.get("/kv", async (c) => {
+  const loadManyHotokes = newLoadManyHotokesWithKv(c.env);
   const hotokes = await loadManyHotokes({});
   return c.json(hotokes);
 });
