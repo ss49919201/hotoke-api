bc45c3677adae7533051e0a7d837df0e79344bc2
ss49919201 <sakashin10291029@gmail.com>
2024-09-15 09:52:11 +0900

    feat: remove kv repository

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,10 @@
 import { Hono } from "hono";
 import { Env, newLoadManyHotokes } from "./infrastructure/d1/repository/hotoke";
 
-import {
-  Env as EnvWithKv,
-  newLoadManyHotokes as newLoadManyHotokesWithKv,
-} from "./infrastructure//kv/reposiotry/hotoke";
-
-const app = new Hono<{ Bindings: Env & EnvWithKv }>();
+const app = new Hono<{ Bindings: Env }>();
 
 app.get("/", async (c) => {
   const loadManyHotokes = newLoadManyHotokes(c.env);
-  const hotokes = await loadManyHotokes({});
-  return c.json(hotokes);
-});
-
-app.get("/kv", async (c) => {
-  const loadManyHotokes = newLoadManyHotokesWithKv(c.env);
   const hotokes = await loadManyHotokes({});
   return c.json(hotokes);
 });
