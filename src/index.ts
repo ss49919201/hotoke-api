3f5bcd296143ea2a8d3833bfaf722726f04a4813
ss49919201 <sakashin10291029@gmail.com>
2024-09-17 09:52:07 +0900

    feat: grouping route

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { Hono } from "hono";
 import { Env, newLoadManyHotokes } from "./infrastructure/d1/repository/hotoke";
 
-const app = new Hono<{ Bindings: Env }>();
-
-app.get("/", async (c) => {
+const hotokeApp = new Hono<{ Bindings: Env }>().get("/", async (c) => {
   const loadManyHotokes = newLoadManyHotokes(c.env);
   const hotokes = await loadManyHotokes({});
   return c.json(hotokes);
 });
+
+const app = new Hono().route("/hotokes", hotokeApp);
 
 export default app;
